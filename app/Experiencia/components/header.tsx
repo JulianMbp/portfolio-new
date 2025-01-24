af1ed02fdbdfa3134b6340fae5384b21484c909e
Julian M. Bastidas P <julian_mtb993@outlook.es>
2025-01-23 20:39:22 -0500

    añadiendo header

---
+++ app/Experiencia/components/header.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Link from "next/link";
+
+
+export default function Header() {
+    return (
+    <div className="my-5 justify-around flex px-32">
+        <div className="flex justify-around w-screen">
+        <button className="h-16 order-2 shadow-2xl drop-shadow-2xl duration-300 flex items-center rounded-3xl p-3 bg-gradient-to-t from-azul-700 via-azul-800 to-azul-900 gap-2 
+            hover:border-hidden hover:scale-110 active:scale-95 active:duration-200 
+            hover:shadow-2xl transition-all ">
+                                    <svg className="h-6 w-6 text-slate-100 " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />
+                        <polyline points="17 6 23 6 23 12" />
+                    </svg>
+        <Link href="/experiencia" className="text-xl text-azul-50 p-2">Experiencia</Link>
+               
+        </button> 
+        <div className="h-16 order-1 shadow-2xl drop-shadow-2xl duration-300 flex items-center rounded-3xl p-3 bg-gradient-to-t from-azul-700 via-azul-800 to-azul-900 gap-3 
+            hover:border-hidden hover:scale-110 active:scale-95 active:duration-200 
+            hover:shadow-2xl transition-all ">
+                                    <svg className="h-5 w-5 text-slate-100 " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                    </svg>
+        <Link href="/" className="text-xl text-azul-50 p-2">Home</Link>
+        </div>
+        
+        <div className="h-16 order-3 shadow-2xl drop-shadow-2xl duration-300 flex items-center rounded-3xl p-3 bg-gradient-to-t from-azul-700 via-azul-800 to-azul-900 gap-3 
+            hover:border-hidden hover:scale-110 active:scale-95 active:duration-200 
+            hover:shadow-2xl transition-all ">
+                                    <svg className="h-5 w-5 text-slate-100 " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                    </svg>
+        <Link href="/proyectos" className="text-xl text-azul-50 p-2">Proyectos</Link>
+        </div>
+        <div className="h-16 order-4 shadow-2xl drop-shadow-2xl duration-300 items-center rounded-3xl p-3 bg-gradient-to-t from-azul-700 via-azul-800 to-azul-900 gap-3 
+            hover:border-hidden hover:scale-110 active:scale-95 active:duration-200 
+            hover:shadow-2xl transition-all flex ">
+                    <svg className="h-5 w-5 text-slate-100 " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                        <circle cx="12" cy="7" r="4" />
+                    </svg>
+        <Link href="/mas-sobre-mi" className="text-xl text-azul-50 p-2">Mas_Sobre_Mi</Link>
+        </div>
+</div>
+
+
+    </div>
+    );
+}
--- pages/experiencia/index.tsx
+++ pages/experiencia/index.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Footer from "@/app/Bienvenida/sections/footer";
+import Header from "@/app/Experiencia/components/header";
 import Experiencia from "@/app/Experiencia/sections/experiencia";
 
 export default function Home() {
     return (
         <section className="bg-gradient-to-tl font-mono from-azul-600 to-azul-200 
                     flex flex-col justify-center items-center overflow-hidden scroll inset-0 transition-all ">
+            <Header></Header>
             <Experiencia></Experiencia>
             <Footer/>
         </section>
