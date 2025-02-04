ce15b72c6e4a3c12f3bfc08de3837cec15d28722
Julian M. Bastidas P <julian_mtb993@outlook.es>
2025-02-03 19:28:57 -0500

    arreglos en video y pets

--- app/mas-sobre-mi/sections/mas-sobre-mi.tsx
+++ app/mas-sobre-mi/sections/mas-sobre-mi.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Presentacion from "@/app/Experiencia/components/card";
 import Pets from '@/app/mas-sobre-mi/sections/pets';
+import Video from "@/app/mas-sobre-mi/sections/video";
 
 
 
@@ -11,8 +12,8 @@ export default function Proyectos(){
                 <Presentacion></Presentacion>
             </div>
             <div className="font-bold text-azul-50 drop-shadow-2xl text-center order-2 col-span-2 justify-center items-center">
-                <div className="flex justify-center items-center mb-[10%] h-full w-full ">
-                    {/* <Video /> */}
+                <div className="flex flex-col gap-y-52 justify-center items-center mb-[10%] h-full w-full ">
+                    <Video/>
                     <Pets/>
                 </div>
             </div>
